d6b463e483f4b78c9da2d39ca3d08c92c7614400
URBAN DECO <urbandeco@MacBook-Air-de-URBAN.local>
2022-11-23 10:19:32 +0100

    last update commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeOneComponent } from './components/pages/home-one/home-one.component';
+
+const routes: Routes = [
+    {path: '', component: HomeOneComponent}
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
